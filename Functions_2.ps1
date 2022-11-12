c7e151eeb3037f656161077b43dc321e56b1ee51
Triton204 <thomas.maclean91@gmail.com~>
2022-11-12 13:45:24 -0500

    new file:   Functions_2.ps1 	new file:   functions.ps1

---
+++ Functions_2.ps1
@@ -0,0 +1,8 @@
+# Microsoft PowerShell script to create a simple function
+# Author: Guy Thomas
+Function Get-BatAvg{
+Param ($Name, $Runs, $Outs)
+$Avg = [int]($Runs / $Outs*100)/100
+Write-Output "$Name's Average = $Avg, $Runs, $Outs"
+}
+Get-BatAvg Bradman 6996 7
